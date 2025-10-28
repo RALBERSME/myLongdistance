f09349c002216d4f1ee8b0900ab5793329f3ab5f
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2025-10-28 22:09:25 +0100

    Add files via upload

---
+++ fukuoka.js
@@ -0,0 +1,6 @@
+const bridge = document.querySelector("#bridge");
+const videos = document.querySelector(".videos");
+function changeMiddle() {
+  bridge.classList.add("invisible");
+  videos.classList.remove("invisible");
+}
