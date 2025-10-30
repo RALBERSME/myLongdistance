4783bb1c8a46ca2e31a66cb85ecaed7fe7a78481
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2025-10-30 14:45:53 +0100

    investition page added

---
+++ investition.js
@@ -0,0 +1,6 @@
+function showTips() {
+  document.querySelector(".solution").style.animation =
+    "showSolution 2s ease forwards";
+  document.querySelector(".leave").style.animation =
+    "showSolution 2s ease forwards";
+}
